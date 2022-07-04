6484024e1331bc2fef617b7990adaa617e8e98de
HiBixby <fpssj@naver.com>
2022-07-04 20:36:43 +0900

    feat: test/login & test/oauth/login 유저 정보 콘솔 출력 기능

--- src/main/java/com/twopow/security/controller/IndexController.java
+++ src/main/java/com/twopow/security/controller/IndexController.java
@@ -1,15 +1,21 @@
 package com.twopow.security.controller;
 
+import com.twopow.security.config.auth.PrincipalDetails;
 import com.twopow.security.model.User;
 import com.twopow.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller//view를 리턴
 public class IndexController {
@@ -18,6 +24,31 @@ public class IndexController {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/test/login")
+    public @ResponseBody String testLogin(Authentication authentication /*, @AuthenticationPrincipal UserDetails userDetails*/ ,
+                                          @AuthenticationPrincipal PrincipalDetails userDetails){
+        //DI(의존성주입), PrincipalDetails가 UserDetails를 implements해서 이렇게 쓰기 가능
+        System.out.println("/test/login/==========");
+        PrincipalDetails principalDetails=(PrincipalDetails)authentication.getPrincipal();//다운캐스팅
+        System.out.println("authentication:"+principalDetails.getUser());
+        //System.out.println("userDetails:"+userDetails.getUsername());
+        System.out.println("userDetails:"+userDetails.getUser());
+        return "세션정보 확인하기";
+    }
+
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody String testOAuthLogin(Authentication authentication, @AuthenticationPrincipal OAuth2User oauth){
+        //Authentication 객체로도 접근 가능하고 @AuthenticationPrincipal 어노테이션으로도 접근 가능
+        //DI(의존성주입)
+        System.out.println("/test/oauth/login/==========");
+        OAuth2User oauth2User =(OAuth2User) authentication.getPrincipal();//다운캐스팅
+
+        System.out.println("authentication:"+oauth2User.getAttributes());
+        System.out.println("oauth2User:"+oauth.getAttributes());
+        return "OAuth 세션정보 확인하기";
+    }
+
     //localhost:8080
     //localhost:8080/
     @GetMapping({"","/"})
