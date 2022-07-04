8ade4c8eee19cc9972bc5f80e846416674da6942
HiBixby <fpssj@naver.com>
2022-07-05 01:36:58 +0900

    feat: 다중 플랫폼을 이용한 소셜 로그인 기능 구현

--- src/main/java/com/twopow/security/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/twopow/security/config/oauth/PrincipalOauth2UserService.java
@@ -1,6 +1,10 @@
 package com.twopow.security.config.oauth;
 
 import com.twopow.security.config.auth.PrincipalDetails;
+import com.twopow.security.config.oauth.provider.FacebookUserInfo;
+import com.twopow.security.config.oauth.provider.GoogleUserInfo;
+import com.twopow.security.config.oauth.provider.NaverUserInfo;
+import com.twopow.security.config.oauth.provider.OAuth2UserInfo;
 import com.twopow.security.model.User;
 import com.twopow.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +15,15 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     @Autowired
     private UserRepository userRepository;
 
     //구글로부터 받은 userRequest 데이터에 대한 후처리되는 함수
+    //함수 종료시 @AuthenticationPrincipal 어노테이션이 만들어진다.
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         //System.out.println("userRequest:"+userRequest.getClientRegistration());//registrationId로 어떤 OAuth로 로그인 했는지 확인 가능.
@@ -27,15 +34,34 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
         //System.out.println("userRequest:"+super.loadUser(userRequest).getAttributes());
         OAuth2User oauth2User=super.loadUser(userRequest);
         System.out.println("getAttributes:"+oauth2User.getAttributes());
-        String provider = userRequest.getClientRegistration().getClientId();//google
-        String providerId=oauth2User.getAttribute("sub");
+
+        OAuth2UserInfo oAuth2UserInfo=null;
+        if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
+            System.out.println("구글 로그인 요청");
+            oAuth2UserInfo=new GoogleUserInfo(oauth2User.getAttributes());
+        }
+        else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")){
+            System.out.println("페이스북 로그인 요청");
+            oAuth2UserInfo=new NaverUserInfo((Map)oauth2User.getAttributes().get("response"));
+        }
+        else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+
+        }
+        else{
+            System.out.println("지원하지 않는 플랫폼 입니다.");
+        }
+
+
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId=oAuth2UserInfo.getProviderId();
         String username=provider+"_"+providerId;
         String password= new BCryptPasswordEncoder().encode("이에이승팀테스트용비밀키");
         String role="ROLE_USER";
-        String email=oauth2User.getAttribute("email");
+        String email=oAuth2UserInfo.getEmail();
 
         User userEntity = userRepository.findByUsername(username);
         if(userEntity==null){ //가입한적 없는경우
+            System.out.println("첫번째 로그인");
             userEntity=User.builder()
                     .username(username)
                     .password(password)
@@ -45,6 +71,9 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
                     .providerId(providerId)
                     .build();
             userRepository.save(userEntity);
+        }
+        else{
+            System.out.println("로그인 이미 한적이 있음");
         }
 
         //return super.loadUser(userRequest);
