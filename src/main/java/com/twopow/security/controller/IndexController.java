be4f4bb7dc7f7551c93b45fbf6356b5be0c2328c
HiBixby <fpssj@naver.com>
2022-07-08 17:15:38 +0900

    feat:cors 설정 추가

--- src/main/java/com/twopow/security/controller/IndexController.java
+++ src/main/java/com/twopow/security/controller/IndexController.java
@@ -11,11 +11,12 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller//view를 리턴
 public class IndexController {
     @Autowired
