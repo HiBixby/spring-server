2d88db517407fddfead7ff7a0744902c66dafccc
HiBixby <fpssj@naver.com>
2022-07-12 13:20:04 +0900

    test: 로그인되어있을경우 /oauth/redirect 에 get요청하면 200반환하는지 확인하는 테스트 추가

---
+++ src/test/java/com/twopow/security/RestControllerTest.java
@@ -0,0 +1,94 @@
+package com.twopow.security;
+
+
+import com.twopow.security.config.SecurityConfig;
+import com.twopow.security.config.auth.PrincipalDetails;
+import com.twopow.security.config.oauth.OAuth2SuccessHandler;
+import com.twopow.security.config.oauth.PrincipalOauth2UserService;
+import com.twopow.security.controller.RestLoginController;
+import com.twopow.security.model.User;
+import com.twopow.security.repository.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithSecurityContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CorsFilter;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import javax.swing.*;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RestControllerTest {
+    @Autowired
+    private WebApplicationContext context;
+    @Autowired
+    private UserRepository userRepository;
+    private MockMvc mvc;
+    private User user;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
+        user = User.builder()
+                .name("홍길동")
+                .username("naver_123456789")
+                .email("RedRoadCopper@naver.com")
+                .role("ROLE_USER")
+                .provider("naver")
+                .providerId("123456789")
+                .picture("https://ssl.pstatic.net/static/newsstand/up/2013/0813/nsd114029379.gif")
+                .build();
+        user = userRepository.save(user);
+    }
+
+
+    @Test
+    public void oauth_인증시200코드() throws Exception {
+        PrincipalDetails principalDetails=new PrincipalDetails(user,null);
+        mvc.perform(get("/oauth/redirect").with(user(principalDetails))).andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+
+    }
+
+}
