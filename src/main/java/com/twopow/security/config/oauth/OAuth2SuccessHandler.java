fc467c2920c5953195fb1cca2199176344b1cc30
HiBixby <fpssj@naver.com>
2022-07-20 00:03:19 +0900

    ✨ refresh Token 생성 후 userRepository 에 저장

--- src/main/java/com/twopow/security/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/com/twopow/security/config/oauth/OAuth2SuccessHandler.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.twopow.security.config.auth.PrincipalDetails;
 import com.twopow.security.jwt.JwtUtil;
 import com.twopow.security.model.User;
+import com.twopow.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -22,12 +23,15 @@ import java.util.Date;
 @RequiredArgsConstructor
 @Component
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final UserRepository userRepository;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException {
         User user = ((PrincipalDetails) authentication.getPrincipal()).getUser();
         String accessToken = JwtUtil.CreateToken(user, JwtUtil.Minutes(30));
         String refreshToken = JwtUtil.CreateToken(user, JwtUtil.Days(14));
+        user.setRefreshToken(refreshToken);
+        userRepository.save(user);
         String targetUrl = UriComponentsBuilder
                 .fromUriString("http://localhost:3000/oauth2/redirect")
                 .queryParam("userToken", accessToken)
