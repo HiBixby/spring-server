58746e4833d540529584ee4cbb89c9d18c38ccc7
HiBixby <fpssj@naver.com>
2022-07-20 04:34:24 +0900

    ✅ /register API에 POST 요청시 DB에 유저 주소 잘 저장되는지 확인하는 테스트코드 추가

--- src/test/java/com/twopow/security/PostApiControllerTest.java
+++ src/test/java/com/twopow/security/PostApiControllerTest.java
@@ -1,20 +1,23 @@
 package com.twopow.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.twopow.security.jwt.JwtUtil;
+import com.twopow.security.model.Register;
 import com.twopow.security.model.User;
+import com.twopow.security.repository.UserRepository;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
@@ -30,31 +33,59 @@ public class PostApiControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user;
+    private String jwtToken;
+
+    @Before
+    public void setup() {
+        user = User.builder()
+                .name("홍길동")
+                .username("naver_123456789")
+                .email("RedRoadCopper@naver.com")
+                .role("ROLE_USER")
+                .provider("naver")
+                .providerId("123456789")
+                .picture("https://ssl.pstatic.net/static/newsstand/up/2013/0813/nsd114029379.gif")
+                .build();
+        userRepository.save(user);
+
+        jwtToken = JwtUtil.CreateToken(user,JwtUtil.Minutes(1));
+    }
+
+    @After
+    public void tearDown() throws Exception{
+        userRepository.deleteAll();
+    }
     @Test
     public void 주소가등록된다() throws Exception {
-        Map<String, String> input = new HashMap<>();
-        input.put("address", "부산");
-        String url = "http://localhost:" + port + "/register";
+
+        //given
+        String address = "주소";
+        Register register = new Register();
+        register.setAddress(address);
+
+        // create headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(jwtToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        // build the request
+        HttpEntity<Register> request = new HttpEntity<>(register,headers);
+
+        String url = "http://localhost:"+port+"/register";
 
         //when
-        ResponseEntity<?> responseEntity = restTemplate.postForEntity(url, input);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url,request,String.class);
+
         //then
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        assertThat(responseEntity.getBody()).isGreaterThan("");
 
-        mvc.perform(post("/register")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(input))
-                        .with(user(principalDetails))
-                        .header("Authorization", "Bearer " + jwtToken))
-                .andExpect(status().isOk());
-
-        Optional<User> userOptional = userRepository.findByProviderAndProviderId(principalDetails.getUser().getProvider(), principalDetails.getUser().getProviderId());
-        if (userOptional.isPresent()) {
-            user = userOptional.get();
-            assertThat(user.getAddress()).isEqualTo(input.get("address"));
-
-
-        }
+        List<User> all = userRepository.findAll();
+        assertThat(all.get(0).getAddress()).isEqualTo(address);
     }
 }
